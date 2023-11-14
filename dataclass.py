505767880d0d4d1f7130c1c6ca9b6183bcc79a32
Mingtao Ou <94691112+Mingtaorwth@users.noreply.github.com>
2023-11-14 06:51:43 +0100

    Add files via upload

---
+++ dataclass.py
@@ -0,0 +1,14 @@
+from torch.utils.data import Dataset
+from torch.utils.data import Dataset
+import os
+
+
+class MyData(Dataset):
+    def __init__(self, root_dir, label_dir):
+        self.root_dir = root_dir
+        self.label_dir = label_dir
+        self.path = os.path.join(self.root_dir, self.label_dir)
+        self.img_path = os.listdir(self.path)
+
+    def __len__(self):
+        return len(self.img_path)
